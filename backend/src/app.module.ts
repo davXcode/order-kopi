581d9a0a7ff68e0789e3badaa3659092bf28af53
David Surya Ardi <david@192.168.1.3>
2026-01-01 04:59:32 +0700

    move to postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 import { OrdersModule } from './orders/orders.module';
-import { Order } from './orders/order.entity';
+// import { Order } from './orders/order.entity';
 
 import { SettingsModule } from './settings/settings.module';
-import { Setting } from './settings/setting.entity';
+// import { Setting } from './settings/setting.entity';
 
 @Module({
   imports: [
@@ -24,16 +24,23 @@ import { Setting } from './settings/setting.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
       autoLoadEntities: true,
-      entities: [Order, Setting],
-      synchronize: true, // kalau production serius, mending pakai migrations
-      ssl:
-        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-      extra:
-        process.env.DB_PGBOUNCER === 'true'
-          ? { max: 1 } // penting di serverless biar gak meledak koneksi
-          : undefined,
+      synchronize: true, // aman untuk project kecil
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   url: process.env.DATABASE_URL,
+    //   autoLoadEntities: true,
+    //   entities: [Order, Setting],
+    //   synchronize: true, // kalau production serius, mending pakai migrations
+    //   ssl:
+    //     process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    //   extra:
+    //     process.env.DB_PGBOUNCER === 'true'
+    //       ? { max: 1 } // penting di serverless biar gak meledak koneksi
+    //       : undefined,
+    // }),
     OrdersModule,
     SettingsModule,
   ],
